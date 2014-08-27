18bf21caae8b37c1968765aae010dba756bef9e2
Mystist <gjl87910lq@hotmail.com>
2014-08-28 01:28:15 +0800

    almost_finished

--- test/hotag_test.js
+++ test/hotag_test.js
@@ -36,5 +36,54 @@
     this.$element.mystistHotag()
     ok(this.$element.children().length > 0, 'container has been created.')
   })
+  
+  test('should expose default settings', function () {
+    ok($.fn.mystistHotag.Constructor.DEFAULTS, 'defaults is defined')
+  })
+  
+  module('utils', {
+    setup: function () {
+      this.$element = $('#qunit-fixture')
+      this.tags = [{"counts":12,"tag":"balabala"},{"counts":4,"tag":"liber"},{"counts":3,"tag":"ranking"},{"counts":11,"tag":"slash"},{"counts":8,"tag":"slide"},{"counts":5,"tag":"tag"}]
+      this.$element.hotag({
+        tags: this.tags
+      })
+      this.hotag =  this.$element.data('mystist.hotag')
+      this.utils = $.fn.hotag.Constructor._private.utils
+    },
+    teardown: function () {
+      var data = this.$element.data('mystist.hotag')
+      if(data) {
+        this.$element.removeData('mystist.hotag')
+      }
+    }
+  })
+  
+  test('should get array from tags', function () {
+    deepEqual(this.utils.getArr.call(this.hotag, this.tags), [12, 4, 3, 11, 8, 5])
+  })
+  
+  test('shoud calcuate points', function () {
+    strictEqual(this.utils.calPoints(1, {mmax: 10, min: 1}), 100)
+    strictEqual(this.utils.calPoints(10, {mmax: 10, min: 1}), 300)
+  })
+  
+  module('helper', {
+    setup: function () {
+      this.helper = $.fn.hotag.Constructor._private.helper
+    }
+  })
+  
+  test('should get m object', function () {
+    var arr = [2, 5, 2, 23, 2, 6, 0, 0, 8, 1, 1]
+    deepEqual(this.helper.m(arr), {max: 23, min: 0})
+  })
+  
+  test('(log n base) should calcuate correctly in Math', function () {
+    strictEqual(this.helper.log(8, 2), 3)
+    strictEqual(this.helper.log(1, 10), 0)
+    strictEqual(this.helper.log(1, 2), 0)
+    strictEqual(this.helper.log(28, 28), 1)
+  })
 
 }(jQuery));
